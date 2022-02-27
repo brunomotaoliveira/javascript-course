401fc57a77c6aad9a8c6928b3586653c3b2054f7
brunomotadev <brunomotadev@gmail.com>
2022-02-26 23:38:11 -0300

    conhecer o escopo globlal e funcoes

---
+++ 052-escopo-global-e-funcoes.js
@@ -0,0 +1,21 @@
+// Declare a variável myGlobal abaixo desta linha
+let myGlobal = 10;
+let oopsGlobal;
+
+function fun1() {
+  // Atribua 5 para oopsGlobal aqui
+oopsGlobal = 5;
+}
+
+// Altere apenas o código acima desta linha
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
