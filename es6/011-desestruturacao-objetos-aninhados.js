0c20e06c9c5b5c1f45aac1818e836be1c63ff1e5
brunomotadev <brunomotadev@gmail.com>
2022-04-04 19:13:37 -0300

    atribuicao de desestruturacao para atribuir variaveis de objetos aninhados

---
+++ es6/011-desestruturacao-objetos-aninhados.js
@@ -0,0 +1,12 @@
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };
+  
+  // Altere apenas o código abaixo desta linha
+  
+  const lowToday = LOCAL_FORECAST.today.low;
+  const highToday = LOCAL_FORECAST.today.high;
+  
+  // Altere apenas o código acima desta linha
